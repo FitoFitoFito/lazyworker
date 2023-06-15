a1ee56325dd834f0991954bb0b31f6979175506c
Fito <code@fitofuente.com>
2023-06-15 17:27:28 +0200

    Fixed the wait() method changing keyElement argument name to conditionArg for clariness, and also adding null as it's pre-default value. I added a 4th condition where conditionArg could be null to execute conditions that do not necessarly need arguments. - Change the time unit in the sleep() method to seconds. - Added some useful comments inside the methods.

--- lazyworker.js
+++ lazyworker.js
@@ -8,13 +8,13 @@ const lazyworker = {
    *
    *  wait() will have 4 parameters:
    *  - condition: It's a Function.
-   *  - keyElement: (string) The element we want to wait for, it depends on the condition.
+   *  - conditionArg: It is the argument we can pass to the condition (if any).
    *  - timeout: It is the max elapsed time to wait for the promise to resolve.
    *  - evalTime: It is the time between evaluations.
    */
-  wait (condition, keyElement, timeout=8000, evalTime=500){
+  wait (condition, conditionArg=null, timeout=3000, evalTime=500){
     return new Promise((res, rej)=>{
-      // Let's define our starting time.
+      // Let's define the starting time.
       const startTime = Date.now();
   
       // This function will be call every "evalTime"ms to evaluate the "condition".
@@ -23,12 +23,16 @@ const lazyworker = {
         // Elapsed time of runCondition().
         const elapsedTime = Date.now() - startTime;
   
-        if(condition(keyElement)){
-          res(condition(keyElement));
+        // If the condition evaluated is not a falsy value then resolve with whatever the condition returns.
+        if(conditionArg !== null && condition(conditionArg)){
+          res(condition(conditionArg));
   
         }else if(elapsedTime >= timeout){
           rej(new Error('Timeout >>> Element not found'));
   
+        }else if(conditionArg === null){
+          res(condition());
+
         }else{
           setTimeout(()=>{runCondition()}, evalTime);
         };
@@ -76,36 +80,44 @@ const lazyworker = {
         rej(new Error('Impossible to click. HTML Element not found'));
       };
 
+      // Get the center coordinates of the element.
       const domRect = HTMLelement.getBoundingClientRect();
       const centerX = domRect.left + domRect.width / 2;
       const centerY = domRect.top + domRect.height / 2;
 
+      // Let's create the mouse event to move the mouse to the center of the element.
       const moveMouse = new MouseEvent('mousemove', {
         clientX: centerX,
         clientY: centerY
       });
 
+      // Let's create the mouse event to click on the element.
       const mouseClick = new MouseEvent('click', {
         bubbles: true,
         cancelable: true,
         view: window
       });
 
+      // Wait for the "mousemove" event to trigger such event.
       HTMLelement.addEventListener('mousemove', res);
+      // Execute the event.
       HTMLelement.dispatchEvent(moveMouse);
 
+      // Wait for the "click" event to trigger such event.
       HTMLelement.addEventListener('click', res);
+      // Execute the event after 200ms.
       setTimeout(()=>{
         HTMLelement.dispatchEvent(mouseClick);
       },200);
     });
   },
 
-  /** The sleep*() function will simulate a stop on the code */
+  /** The sleep(seconds) function will simulate a stop in the code */
   sleep (time){
     return new Promise(res=>{
-      setTimeout(()=>{res()}, time);
+      setTimeout(()=>{res()}, time*1000);
     });
   }
 };
 
+lazyworker.sl
